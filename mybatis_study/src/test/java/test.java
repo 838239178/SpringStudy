348d34c91a723cd34f77aa9230c9471687aaccff
ShiJh <nnocreater@hotmail.com>
2021-03-08 18:34:15 +0800

    1

---
+++ mybatis_study/src/test/java/test.java
@@ -0,0 +1,9 @@
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+public class test {
+    @Test
+    public void test() throws Exception {
+        SqlSession sqlSession;
+    }
+}
